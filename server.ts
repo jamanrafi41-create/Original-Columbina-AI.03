5634d61f640e9e88adc230bc0eee2b25ad620b05
White Dragon <jamanrafi41@gmail.com>
2026-09-06 23:24:33 +0600

    Update server.ts

--- server.ts
+++ server.ts
@@ -14,7 +14,7 @@ import { groqProvider } from "./server/providers/groq";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.use(express.json({ limit: "15mb" }));
 
